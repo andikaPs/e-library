f86715bad9bce383e0b97e796a07206dd932865d
AndikaPermanaSidiq01 <andikapermanasidiq00@gmail.com>
2021-04-09 23:58:08 +0700

    add login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
           100: "#1B1C1E",
           200: "#D0D0D037",
           300: "#FDFDFD",
+          400: "#F5F2F2",
         },
         warning: {
           100: "#FFA50085",
