92d337a8e8021caf4f1cb0389db070931c89d353
AndikaPermanaSidiq01 <andikapermanasidiq00@gmail.com>
2021-05-14 21:17:33 +0700

    add profile page dan detail buku diperbarui

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,17 @@ $(document).ready(function () {
     e.preventDefault();
     $("#dropdown").toggleClass("hidden");
   });
+
+  $("#wishlist").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("bg-gray-400");
+    $(this).toggleClass("bg-red-600");
+  });
+
+  let text = $(".judul").text();
+  if (text.length > 22) {
+    const cutText = text.slice(0, 20) + "....";
+    $(".judul").text(cutText);
+  }
+  console.log($(".judul").text().length);
 });
